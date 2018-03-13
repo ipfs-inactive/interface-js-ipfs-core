274623f5089538aa80b6134d49dbad1c7fb3a826
Volker Mische <volker.mische@gmail.com>
2018-03-13 14:00:43 +0100

    Revert "feat: complete files.stat with the 'with-local' option (#227)"
    This reverts commit 5969fed4c83c0b96ae437d7c0ed31c1f5bf5498b.

--- js/src/files-mfs.js
+++ js/src/files-mfs.js
@@ -276,10 +276,7 @@ module.exports = (common) => {
             blocks: 1,
             size: 13,
             hash: 'QmcZojhwragQr5qhTeFAmELik623Z21e3jBTpJXoQ9si1T',
-            cumulativeSize: 71,
-            withLocality: false,
-            local: undefined,
-            sizeLocal: undefined
+            cumulativeSize: 71
           })
           done()
         })
@@ -298,54 +295,7 @@ module.exports = (common) => {
             blocks: 2,
             size: 0,
             hash: 'QmVrkkNurBCeJvPRohW5JTvJG4AxGrFg7FnmsZZUS6nJto',
-            cumulativeSize: 216,
-            withLocality: false,
-            local: undefined,
-            sizeLocal: undefined
-          })
-          done()
-        })
-      })
-
-      it('stat withLocal file', function (done) {
-        if (!withGo) {
-          console.log('Not supported in js-ipfs yet')
-          this.skip()
-        }
-
-        ipfs.files.stat('/test/b', {'withLocal': true}, (err, stat) => {
-          expect(err).to.not.exist()
-          expect(stat).to.eql({
-            type: 'file',
-            blocks: 1,
-            size: 13,
-            hash: 'QmcZojhwragQr5qhTeFAmELik623Z21e3jBTpJXoQ9si1T',
-            cumulativeSize: 71,
-            withLocality: true,
-            local: true,
-            sizeLocal: 71
-          })
-          done()
-        })
-      })
-
-      it('stat withLocal dir', function (done) {
-        if (!withGo) {
-          console.log('Not supported in js-ipfs yet')
-          this.skip()
-        }
-
-        ipfs.files.stat('/test', {'withLocal': true}, (err, stat) => {
-          expect(err).to.not.exist()
-          expect(stat).to.eql({
-            type: 'directory',
-            blocks: 2,
-            size: 0,
-            hash: 'QmVrkkNurBCeJvPRohW5JTvJG4AxGrFg7FnmsZZUS6nJto',
-            cumulativeSize: 216,
-            withLocality: true,
-            local: true,
-            sizeLocal: 216
+            cumulativeSize: 216
           })
           done()
         })
