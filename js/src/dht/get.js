suxinyan/interface-ipfs-core

fix: update dht tests

js/src/dht/get.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 'use strict'
 
+const hat = require('hat')
 const waterfall = require('async/waterfall')
 const { spawnNodesWithId } = require('../utils/spawn')
 const { getDescribe, getIt, expect } = require('../utils/mocha')
@@ -30,7 +31,6 @@ module.exports = (createCommon, options) => {
 
           nodeA = nodes[0]
           nodeB = nodes[1]
-
           connect(nodeA, nodeB.peerId.addresses[0], done)
         })
       })
@@ -39,28 +39,53 @@ module.exports = (createCommon, options) => {
     after((done) => common.teardown(done))
 
     it('should error when getting a non-existent key from the DHT', (done) => {
-      nodeA.dht.get('non-existing', { timeout: '100ms' }, (err, value) => {
+      nodeA.dht.get('non-existing', { timeout: '100ms' }, (err) => {
         expect(err).to.be.an.instanceof(Error)
         done()
       })
     })
 
-    it('should get a value after it was put on another node', function (done) {
+    it('should get a value after it was added on another node', function (done) {
       this.timeout(80 * 1000)
 
-      // TODO - this test needs to keep tryingl instead of the setTimeout
       waterfall([
-        (cb) => nodeB.object.new('unixfs-dir', cb),
+        (cb) => nodeB.object.put(Buffer.from(hat()), cb),
         (dagNode, cb) => setTimeout(() => cb(null, dagNode), 20000),
         (dagNode, cb) => {
           const multihash = dagNode.toJSON().multihash
 
-          nodeA.dht.get(multihash, cb)
+          nodeA.object.get(multihash, cb)
         },
         (result, cb) => {
-          expect(result).to.eql('')
+          expect(result).to.exist()
           cb()
         }
+      ], done)
+    })
+
+    it('should get a value after it was put on another node', function (done) {
+      this.timeout(80 * 1000)
+      const multihash = Buffer.from('/v/hello')
+      const data = Buffer.from('data')
+
+      // Rewrite validators to simply validate the record
+      nodeA._libp2pNode._dht.validators.v = nodeB._libp2pNode._dht.validators.v = {
+        func (key, publicKey, callback) {
+          setImmediate(callback)
+        },
+        sign: false
+      }
+
+      // Rewrite selectors to select first received record
+      nodeA._libp2pNode._dht.selectors.v = () => 0
+
+      waterfall([
+        (cb) => nodeB.dht.put(multihash, data, cb),
+        (cb) => nodeA.dht.get(multihash, (err, res) => {
+          expect(err).to.not.exist()
+          expect(res).to.eql(data)
+          cb()
+        })
       ], done)
     })
   })

ipfs-inactive/interface-js-ipfs-core

js/src/dht/get.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 'use strict'
 
+const hat = require('hat')
 const waterfall = require('async/waterfall')
 const { spawnNodesWithId } = require('../utils/spawn')
 const { getDescribe, getIt, expect } = require('../utils/mocha')
@@ -30,7 +31,6 @@ module.exports = (createCommon, options) => {
 
           nodeA = nodes[0]
           nodeB = nodes[1]
-
           connect(nodeA, nodeB.peerId.addresses[0], done)
         })
       })
@@ -39,28 +39,53 @@ module.exports = (createCommon, options) => {
     after((done) => common.teardown(done))
 
     it('should error when getting a non-existent key from the DHT', (done) => {
-      nodeA.dht.get('non-existing', { timeout: '100ms' }, (err, value) => {
+      nodeA.dht.get('non-existing', { timeout: '100ms' }, (err) => {
         expect(err).to.be.an.instanceof(Error)
         done()
       })
     })
 
-    it('should get a value after it was put on another node', function (done) {
+    it('should get a value after it was added on another node', function (done) {
       this.timeout(80 * 1000)
 
-      // TODO - this test needs to keep tryingl instead of the setTimeout
       waterfall([
-        (cb) => nodeB.object.new('unixfs-dir', cb),
+        (cb) => nodeB.object.put(Buffer.from(hat()), cb),
         (dagNode, cb) => setTimeout(() => cb(null, dagNode), 20000),
         (dagNode, cb) => {
-          const multihash = dagNode.toJSON().multihash
+          const hash = dagNode.toJSON().hash
 
-          nodeA.dht.get(multihash, cb)
+          nodeA.object.get(hash, cb)
         },
         (result, cb) => {
-          expect(result).to.eql('')
+          expect(result).to.exist()
           cb()
         }
+      ], done)
+    })
+
+    it('should get a value after it was put on another node', function (done) {
+      this.timeout(80 * 1000)
+      const multihash = Buffer.from('/v/hello')
+      const data = Buffer.from('data')
+
+      // Rewrite validators to simply validate the record
+      nodeA._libp2pNode._dht.validators.v = nodeB._libp2pNode._dht.validators.v = {
+        func (key, publicKey, callback) {
+          setImmediate(callback)
+        },
+        sign: false
+      }
+
+      // Rewrite selectors to select first received record
+      nodeA._libp2pNode._dht.selectors.v = () => 0
+
+      waterfall([
+        (cb) => nodeB.dht.put(multihash, data, cb),
+        (cb) => nodeA.dht.get(multihash, (err, res) => {
+          expect(err).to.not.exist()
+          expect(res).to.eql(data)
+          cb()
+        })
       ], done)
     })
   })