66da2b0a9833121f175c9dc3b3c17e527a586d4a
Alan Shaw <alan.shaw@protocol.ai>
2018-12-05 21:16:00 +0000

    refactor: convert utils to use async/await (#390)
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- js/src/utils/swarm.js
+++ js/src/utils/swarm.js
@@ -1,20 +1,18 @@
 'use strict'
 
-const eachSeries = require('async/eachSeries')
+const pause = ms => new Promise((resolve, reject) => setTimeout(resolve, ms))
 
-function connect (fromNode, toAddrs, cb) {
+async function connect (fromNode, toAddrs, cb) {
   if (!Array.isArray(toAddrs)) {
     toAddrs = [toAddrs]
   }
 
   // FIXME ??? quick connections to different nodes sometimes cause no
   // connection and no error, hence serialize connections and pause between
-  eachSeries(toAddrs, (toAddr, cb) => {
-    fromNode.swarm.connect(toAddr, (err) => {
-      if (err) return cb(err)
-      setTimeout(cb, 300)
-    })
-  }, cb)
+  for (let i = 0; i < toAddrs.length; i++) {
+    await fromNode.swarm.connect(toAddrs[i])
+    await pause(300)
+  }
 }
 
 module.exports.connect = connect
