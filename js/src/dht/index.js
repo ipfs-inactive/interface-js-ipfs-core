de4284ea8519a8c64a50129eb41a3ab60371f442
Alan Shaw <alan.shaw@protocol.ai>
2018-12-14 09:06:53 +0000

    fix: path
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- js/src/dht/index.js
+++ js/src/dht/index.js
@@ -4,9 +4,9 @@ const { createSuite } = require('../utils/suite')
 const tests = {
   get: require('./get'),
   put: require('./put'),
-  findPeer: require('./findPeer'),
+  findPeer: require('./find-peer'),
   provide: require('./provide'),
-  findProvs: require('./findProvs'),
+  findProvs: require('./find-provs'),
   query: require('./query')
 }
 
