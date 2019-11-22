20e842becd1a7c582fbff3f16306e90db525a90c
Pedro Santos <pedro.santos@moxy.studio>
2019-11-22 15:57:52 +0000

    refactor: bootstrap before and after methods to async syntax

--- src/bootstrap/rm.js
+++ src/bootstrap/rm.js
@@ -16,22 +16,11 @@ module.exports = (createCommon, options) => {
 
     let ipfs
 
-    before(function (done) {
-      // CI takes longer to instantiate the daemon, so we need to increase the
-      // timeout for the before step
-      this.timeout(60 * 1000)
-
-      common.setup((err, factory) => {
-        expect(err).to.not.exist()
-        factory.spawnNode((err, node) => {
-          expect(err).to.not.exist()
-          ipfs = node
-          done()
-        })
-      })
+    before(async () => {
+      ipfs = await common.setup()
     })
 
-    after((done) => common.teardown(done))
+    after(() => common.teardown())
 
     it('should return an error when called with an invalid arg', () => {
       return expect(ipfs.bootstrap.rm(invalidArg)).to.eventually.be.rejected
