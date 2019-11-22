187c89e0cfe2cc497f4b2d10ee37d32ed7fa5af4
Pedro Santos <pedro.santos@moxy.studio>
2019-11-22 16:02:24 +0000

    refactor: object before and after methods to async syntax

--- src/object/new.js
+++ src/object/new.js
@@ -13,22 +13,11 @@ module.exports = (createCommon, options) => {
 
     let ipfs
 
-    before(function (done) {
-      // CI takes longer to instantiate the daemon, so we need to increase the
-      // timeout for the before step
-      this.timeout(60 * 1000)
-
-      common.setup((err, factory) => {
-        expect(err).to.not.exist()
-        factory.spawnNode((err, node) => {
-          expect(err).to.not.exist()
-          ipfs = node
-          done()
-        })
-      })
+    before(async () => {
+      ipfs = await common.setup()
     })
 
-    after((done) => common.teardown(done))
+    after(() => common.teardown())
 
     it('should create a new object with no template', async () => {
       const cid = await ipfs.object.new()
