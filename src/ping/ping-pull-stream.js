9953c7fff1e013013a03bc3df6d2d292668e088c
Pedro Santos <pedro.santos@moxy.studio>
2019-11-22 16:02:49 +0000

    refactor: ping before and after methods to async syntax

--- src/ping/ping-pull-stream.js
+++ src/ping/ping-pull-stream.js
@@ -2,11 +2,8 @@
 'use strict'
 
 const pullToPromise = require('pull-to-promise')
-const series = require('async/series')
-const { spawnNodesWithId } = require('../utils/spawn')
 const { getDescribe, getIt, expect } = require('../utils/mocha')
 const { isPong } = require('./utils.js')
-const { connect } = require('../utils/swarm')
 
 module.exports = (createCommon, options) => {
   const describe = getDescribe(options)
@@ -14,31 +11,18 @@ module.exports = (createCommon, options) => {
   const common = createCommon()
 
   describe('.pingPullStream', function () {
-    // TODO revisit when https://github.com/ipfs/go-ipfs/issues/5799 is resolved
-    this.timeout(2 * 60 * 1000)
+    this.timeout(60 * 1000)
 
     let ipfsA
     let ipfsB
 
-    before(function (done) {
-      common.setup((err, factory) => {
-        if (err) return done(err)
-
-        series([
-          (cb) => {
-            spawnNodesWithId(2, factory, (err, nodes) => {
-              if (err) return cb(err)
-              ipfsA = nodes[0]
-              ipfsB = nodes[1]
-              cb()
-            })
-          },
-          (cb) => connect(ipfsA, ipfsB.peerId.addresses[0], cb)
-        ], done)
-      })
+    before(async () => {
+      ipfsA = await common.setup()
+      ipfsB = await common.setup()
+      await ipfsA.swarm.connect(ipfsB.peerId.addresses[0])
     })
 
-    after((done) => common.teardown(done))
+    after(() => common.teardown())
 
     it('should send the specified number of packets over pull stream', async () => {
       const count = 3
