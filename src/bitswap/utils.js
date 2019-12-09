6881e7274fb8c0e91eceeeaf862d7de3dffc4281
achingbrain <alex@achingbrain.net>
2019-12-09 15:23:21 +0000

    fix: simpler wait for want list

--- src/bitswap/utils.js
+++ src/bitswap/utils.js
@@ -1,24 +1,23 @@
 'use strict'
 
-const pWhilst = require('p-whilst')
+const delay = require('delay')
 
-function waitForWantlistKey (ipfs, key, opts = {}) {
+async function waitForWantlistKey (ipfs, key, opts = {}) {
   opts.timeout = opts.timeout || 10000
-
-  let list = { Keys: [] }
-
   const start = Date.now()
-  const findKey = () => !list.Keys.some(k => k['/'] === key)
+  const end = start + opts.timeout
 
-  const iteratee = async () => {
-    if (Date.now() - start > opts.timeout) {
-      throw new Error(`Timed out waiting for ${key} in wantlist`)
+  while (Date.now() < end) {
+    const list = await ipfs.bitswap.wantlist(opts.peerId)
+
+    if (list.Keys.some(k => k['/'] === key)) {
+      return
     }
 
-    list = await ipfs.bitswap.wantlist(opts.peerId)
+    delay(500)
   }
 
-  return pWhilst(findKey, iteratee)
+  throw new Error(`Timed out waiting for ${key} in wantlist`)
 }
 
 module.exports.waitForWantlistKey = waitForWantlistKey
