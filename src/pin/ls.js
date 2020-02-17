5b15bc12c1c2ddb3276949436f31906a5e247d03
achingbrain <alex@achingbrain.net>
2020-02-17 13:11:17 -0500

    chore: fix linting

--- src/pin/ls.js
+++ src/pin/ls.js
@@ -4,7 +4,6 @@
 const { fixtures } = require('./utils')
 const { getDescribe, getIt, expect } = require('../utils/mocha')
 const all = require('it-all')
-const last = require('it-last')
 const drain = require('it-drain')
 
 /** @typedef { import("ipfsd-ctl/src/factory") } Factory */
